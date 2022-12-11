d7500e95bfbd1a64fe0671332eb82158dc8db213
rock-z <rockzhangenyann@gmail.com>
2022-12-11 07:15:59 -0500

    model trains to better-than-chance accuracy, still not satisfactory.

--- model.py
+++ model.py
@@ -4,11 +4,19 @@ from tensorflow.keras.layers import Layer, Dense, Dropout
 from tensorflow.keras.activations import softmax, relu, sigmoid
 
 class MatchNet(Model):
+    """Subnetwork for attention between P/Q/O pairs. Returns the attention
+    values of the second input from the first, using input directly as query/
+    values
+    
+    Attributes:
+        key_layer: layer to compute keys from input p
+        dropout: dropout rate, if used in training
+    """
     def __init__(self, config, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
         self.key_layer = Dense(units=config['hidden_size'])
-        self.dropout = Dropout(rate=config['dropout_rate'])
+        self.dropout = Dropout(rate=config['dropout'])
     
     def call(self, inputs):
         p, q = inputs
@@ -23,7 +31,7 @@ class MatchNet(Model):
         # Finally get attentionized output of q
         outputs = relu(tf.matmul(attention_weights, q))
         
-        return outputs      
+        return outputs
         
 
 class GateLayers(Model):
@@ -64,35 +72,49 @@ class DCMN(Model):
         # Initialize parameters relevant to task & trainingt
         self.n_choices = config['n_choices']
         self.dropout = config['dropout']
+        self.hidden_size = config['hidden_size']
         
         # Initialize layers
-        self.bert = config['pre_trained_bert']
-        self.classifier = config['classifier']
-        self.gate_layer = config['gate_layer'](config)
+        self.bert_preprocess = config['bert_preprocess']
+        self.bert = config['bert']
         self.match_layer = MatchNet(config)
+        self.gate_layer = config['gate_layer'](config)
+        self.classifier = config['classifier']
         
         
     def call(self, inputs):
+        batch_size = len(inputs)
         passage, question, options = inputs
         
-        p_embedding = self.bert(passage)
-        q_embedding = self.bert(question)
-        o_embedding = self.bert(options)
+        p_tokens = self.bert_preprocess(passage)
+        q_tokens = self.bert_preprocess(question)
+        o_tokens = self.bert_preprocess(options)
+        #tf.map_fn(lambda option: self.bert_preprocess(option), options)
+        #[self.bert_preprocess(option) for option in options]
         
-        po = tf.math.reduce_max(self.match_layer(p_embedding, o_embedding), axis = 1)
-        op = tf.math.reduce_max(self.match_layer(o_embedding, p_embedding), axis = 1)
-        qo = tf.math.reduce_max(self.match_layer(q_embedding, o_embedding), axis = 1)
-        oq = tf.math.reduce_max(self.match_layer(o_embedding, q_embedding), axis = 1)
-        pq = tf.math.reduce_max(self.match_layer(p_embedding, q_embedding), axis = 1)
-        qp = tf.math.reduce_max(self.match_layer(q_embedding, p_embedding), axis = 1)
+        #p_embedding = tf.stack([self.bert(p_tokens)['sequence_output']] * self.n_choices, axis=1)
+        p_embedding = self.bert(p_tokens)['sequence_output']
+        #q_embedding = tf.stack([self.bert(q_tokens)['sequence_output']] * self.n_choices, axis =1)
+        q_embedding = self.bert(q_tokens)['sequence_output']
+        o_embedding = self.bert(o_tokens)['sequence_output']
+        #o_embedding = tf.reshape(self.bert(o_tokens)['sequence_output'], (batch_size, self.n_choices, -1, self.hidden_size))
+        #tf.map_fn(lambda option: self.bert(option), o_tokens)
+        #tf.convert_to_tensor([self.bert(option)['sequence_output'] for option in o_tokens])
+        
+        po = tf.math.reduce_max(self.match_layer((p_embedding, o_embedding)), axis = -2)
+        op = tf.math.reduce_max(self.match_layer((o_embedding, p_embedding)), axis = -2)
+        qo = tf.math.reduce_max(self.match_layer((q_embedding, o_embedding)), axis = -2)
+        oq = tf.math.reduce_max(self.match_layer((o_embedding, q_embedding)), axis = -2)
+        pq = tf.math.reduce_max(self.match_layer((p_embedding, q_embedding)), axis = -2)
+        qp = tf.math.reduce_max(self.match_layer((q_embedding, p_embedding)), axis = -2)
         
         po_fused = self.gate_layer((po, op))
         pq_fused = self.gate_layer((pq, qp))
         qo_fused = self.gate_layer((qo, oq))
         
-        all_attention_values = tf.concat((po_fused, pq_fused, qo_fused), axis=1)
-        
-        answer_logits = tf.reshape(self.classifier(all_attention_values), shape=(-1,self.n_choices))
+        all_attention_values = tf.concat((po_fused, pq_fused, qo_fused), axis=-1)
+        #answer_logits = tf.reshape(self.classifier(all_attention_values), shape=(-1, self.n_choices))
+        answer_logits = self.classifier(all_attention_values)
         
         return answer_logits
     
@@ -109,8 +131,3 @@ class DCMN(Model):
         
     if __name__ == "__main__":
         main()
-        
-        
-        
-        
-        
